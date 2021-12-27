8888ef39bf8f1b6afaf41eacf13ce05854058e43
benjamin <adolphe906@gmail.com>
2021-12-27 17:07:51 +0400

    create home.js

---
+++ assets/home.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("#shrotenform");
+const shortenCard = document.querySelector("#shortenCard");
+const inputURL = document.querySelector("#url");
+const shortenBtn = document.querySelector("#btnShortenUrl");
